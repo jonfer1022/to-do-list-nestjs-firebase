ee8e1c0886a6f6ffd3682594c945db6c92cd8000
Jonathan Amaya <jonathanfab92@gmail.com>
2024-03-26 01:18:36 -0500

    Last changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { PreAuthMiddleware } from './auth/preAuth.middelware';
+import { FirebaseAdmin } from './common/firebaseAdmin.setup';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { Firebase } from './common/firebase.setup';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AuthController, TasksController],
+  providers: [FirebaseAdmin, AuthService, Firebase, TasksService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(PreAuthMiddleware)
+      .exclude('/auth/signup', '/auth/signin')
+      .forRoutes('*');
+  }
+}
